cc9ef8a30386f9de83383a639c5674723359717e
Juan Esteban Aristizabal <jeaz.git@gmail.com>
2021-05-26 17:57:09 -0500

    chore: added scripts to delete all tables in db

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,7 +1,3 @@
-from typing import List, Optional
-
-from sqlalchemy.orm import Session
-
 from app.crud.base import CRUDBase
 from app.schemas import UserCreate, UserUpdate
 from app.models import User
